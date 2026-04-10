a093cac8af6d1e161edd7080cc97deca267ca844
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-10 09:41:16 +0530

    Add maxProfit method for stock price analysis
    Implement maxProfit method to calculate maximum profit from stock prices using a greedy approach.

--- Greedy-Algorithm/maxProfit.java
+++ Greedy-Algorithm/maxProfit.java
@@ -1,1 +1,16 @@
-
+//Here the Key Greedy Idea is that you only need Minimum Price so Far and Maximum Profit because best Profit = sell at today's price - lowest price before today
+//TC - O(n)
+class Solution {
+    public int maxProfit(int[] prices) {
+       int min = Integer.MAX_VALUE;
+       int max = 0;
+       for(int price : prices){
+        if(price < min){
+            min = price;
+        }else{
+            max = Math.max(max,price-min);
+        }
+       }
+    return max;      
+    }
+}
