98d0920c29d2bdbcfd62ef08b9855bc7c19074ea
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-11 18:10:20 +0530

    Update Best-time-to-buy-and-sell-stock-2.java

--- Greedy-Algorithm/Best-time-to-buy-and-sell-stock-2.java
+++ Greedy-Algorithm/Best-time-to-buy-and-sell-stock-2.java
@@ -1,1 +1,15 @@
-
+//Question-Best time to buy and Sell Stock 2(Leetcode-122 Medium)
+class Solution {
+    public int maxProfit(int[] prices) {
+        int total = 0;
+        int start = prices[0];
+        for(int i=1; i<prices.length; i++){
+            int end = prices[i];
+            if(end>=start){
+                total += (end-start);
+            }
+         start = end;   
+        }
+    return total;    
+    }
+}  
