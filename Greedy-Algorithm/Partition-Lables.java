d284e49b1293b7e430d0c81f6e959a5206dbad00
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-11 11:22:43 +0530

    Update Partition-Lables.java

--- Greedy-Algorithm/Partition-Lables.java
+++ Greedy-Algorithm/Partition-Lables.java
@@ -1,1 +1,17 @@
-
+//Question - Leetcode(763 Medium)
+//Here Make use of two pointers start and end and use LastIndexOf() Method to update end limit of the current partition at each character, length of the current partition is end-start+1
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+       int start = 0;
+       int end = 0;
+       ArrayList<Integer> list = new ArrayList<>();
+       for(int i=0; i<s.length(); i++){
+        end = Math.max(end,s.lastIndexOf(s.charAt(i)));
+        if(i == end){
+            list.add(end-start+1);
+            start = i+1;
+        }
+       }
+    return list;   
+    }
+}
