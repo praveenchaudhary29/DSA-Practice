8e1ba263049a428226929082b844909961f979c8
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-14 11:40:22 +0530

    Create Remove-Linked-List-Elements.java

---
+++ Linked-List/Remove-Linked-List-Elements.java
@@ -0,0 +1,24 @@
+//Question - Leetcode(203 Easy)
+//Here we have used simple two pointer method to remove all the nodes from the list where node.val == val and at the end return the head of modified list
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+      ListNode start = head;
+      while(start != null && start.val == val){
+        start = start.next;
+      }
+      if(start == null) return start;
+      ListNode current = start.next;
+      ListNode prev = start;
+      while(current != null){
+        if(current.val == val){
+            prev.next = current.next;
+            current = current.next;
+            if(current == null) break;
+            continue;
+        }
+        prev = prev.next;
+        current = current.next;
+      }
+    return start;   
+    }
+}
