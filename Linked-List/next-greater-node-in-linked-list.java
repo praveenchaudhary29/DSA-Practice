b5a4f9de87b4c53fe2ebb9d1f787c8f6dff578ed
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-15 11:13:49 +0530

    Create next-greater-node-in-linked-list.java

---
+++ Linked-List/next-greater-node-in-linked-list.java
@@ -0,0 +1,31 @@
+//Question - Leetcode(1019 Medium)
+//Use Increasing Monotonic Stack to Find next Greater Node in Linked List
+class Solution {
+    public int[] nextLargerNodes(ListNode head) {
+       ListNode temp =head;
+       int size = 0;
+       ArrayList<Integer> list = new ArrayList<>();
+       while(temp != null){
+        list.add(temp.val);
+        temp = temp.next;
+        size++;
+       }
+       Integer [] linked = list.toArray(new Integer[0]);
+       int [] answer = new int[size];
+       Arrays.fill(answer,0);
+
+       Stack<Integer> stack = new Stack<>();
+
+       for(int i=0; i<linked.length; i++){
+
+        while(!stack.isEmpty() && linked[stack.peek()] < linked[i]){
+            answer[stack.pop()] = linked[i];
+        }
+        stack.push(i);
+       }
+       
+       
+
+    return answer;
+    }
+}
