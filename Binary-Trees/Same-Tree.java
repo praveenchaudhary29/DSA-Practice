04b10b7ff9e2545cf0e12cfec60be413d00b76f8
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-16 20:53:13 +0530

    Create Same-Tree.java

---
+++ Binary-Trees/Same-Tree.java
@@ -0,0 +1,17 @@
+//Question - Leetcode(100 Easy)
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+       if(p==null && q==null){
+           return true; 
+        }
+        if( p == null || q==null){
+            return false;
+        }
+        if(p.val == q.val){
+         boolean left = isSameTree(p.left,q.left);
+        boolean right = isSameTree(p.right,q.right);
+        return left && right;   
+        }
+        return false;
+    }
+    }
