afbc80c3043aa88a46b59191a9a32ed025a49b42
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-19 23:48:23 +0530

    Create Partition-equals-subset-sum.java

---
+++ Dynamic-Programming/Partition-equals-subset-sum.java
@@ -0,0 +1,24 @@
+//Question - Leetcode(416 Medium)
+class Solution {
+    public boolean canPartition(int[] nums) {
+      int sum = 0;
+      for(int num : nums){
+        sum += num;
+      }
+      if(sum % 2 != 0)return false;
+      Boolean dp[][] = new Boolean[nums.length][sum/2 + 1];
+      return possible(nums,0,dp,sum/2);
+    }
+
+    public boolean possible(int [] nums, int i,Boolean [][] dp, int target){
+        if(target == 0) return true;
+        if(target < 0) return false;
+        if(i == nums.length){
+            return false;
+        }
+        if(dp[i][target] != null) return dp[i][target];
+        boolean take = possible(nums,i+1,dp,target-nums[i]);
+        boolean notTake = possible(nums,i+1,dp,target);
+        return dp[i][target] = take || notTake;
+    }
+}
