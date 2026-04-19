94715ebed9f5e272371cd506797fe8c97fd4b6ad
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-19 23:50:04 +0530

    Update Partition-equals-subset-sum.java

--- Dynamic-Programming/Partition-equals-subset-sum.java
+++ Dynamic-Programming/Partition-equals-subset-sum.java
@@ -4,7 +4,7 @@
 class Solution {
     public boolean canPartition(int[] nums) {
       int sum = 0;
-      for(int num : nums){
+      for(int num : nums){ 
         sum += num;
       }
       if(sum % 2 != 0)return false;
