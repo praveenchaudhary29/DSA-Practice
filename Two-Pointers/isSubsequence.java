5b3dcc0f84e3e1127505421fca9c89e75c58ea99
Praveen Chaudhary <pvnchaudhary1604@gmail.com>
2026-04-20 23:17:50 +0530

    Create isSubsequence.java

---
+++ Two-Pointers/isSubsequence.java
@@ -0,0 +1,17 @@
+//Question - Leetcode(392 Easy)
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+       if(s.length()==0)return true;
+       if(t.length()==0)return false;
+       int i = 0;
+       int j = 0;
+       while(j<t.length()){
+        if(s.charAt(i) == t.charAt(j)){
+           i++;
+        }
+        if(i == s.length())return true;
+        j++;
+       } 
+    return false;   
+    }
+}
